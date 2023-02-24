0fdd5319cbb58642f0849d42e7f76d9ddd589064
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-02-24 18:51:03 +0200

    model-answer

--- client.py
+++ client.py
@@ -43,6 +43,7 @@ def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
     if (price_b == 0):
+        # avoid exception error
         return
     return (price_a / price_b)
 
